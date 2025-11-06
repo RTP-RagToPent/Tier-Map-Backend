652817fa824d34f4757d90db61e442727046d77a
kmjak <tymkoron1221@icloud.com>
2025-11-06 09:43:31 +0900

    feat: RalliesControllerの実装
    内容
    - RallesControllerを追加

---
+++ supabase/controller/RalliesController.ts
@@ -0,0 +1,37 @@
+import { SupabaseRallyRepository } from "../infrastructure/supabase/repository/rally.ts";
+import {
+  CreateRallyUseCase,
+  type CreateRallyUseCaseRequest,
+} from "../usecase/rally/CreateUseCase.ts";
+import { ListRallyUseCase } from "../usecase/rally/ListUseCase.ts";
+import { ShowRallyUseCase } from "../usecase/rally/ShowUseCase.ts";
+import {
+  UpdateRallyUseCase,
+  type UpdateRallyUseCaseRequest,
+} from "../usecase/rally/UpdateUseCase.ts";
+
+class RalliesController {
+  constructor(private repository = new SupabaseRallyRepository()) {}
+
+  async index({ profileId }: { profileId: number }) {
+    const useCase = new ListRallyUseCase(this.repository);
+    return await useCase.execute(profileId);
+  }
+
+  async create(request: CreateRallyUseCaseRequest) {
+    const useCase = new CreateRallyUseCase(this.repository);
+    return await useCase.execute(request);
+  }
+
+  async show(rallyId: number) {
+    const useCase = new ShowRallyUseCase(this.repository);
+    return await useCase.execute(rallyId);
+  }
+
+  async update(request: UpdateRallyUseCaseRequest) {
+    const useCase = new UpdateRallyUseCase(this.repository);
+    return await useCase.execute(request);
+  }
+}
+
+export { RalliesController };
